5e8c874948bf6ae6787da6afe89e227e877980d7
Leonard Pauli <leonardpauli@me.com>
2018-09-16 13:18:34 +0200

    EdgeType: turning

--- src/com/leonardpauli/experiments/boardgame/chess/Edge.java
+++ src/com/leonardpauli/experiments/boardgame/chess/Edge.java
@@ -27,12 +27,45 @@ public enum EdgeType {
 	public final char code;
 	public final Point direction;
 	public final String title;
+	private final EdgeType turned;
 
 	private EdgeType(char code, int[2] dir, String title) {
 		this.code = code;
 		this.direction = new Point(dir[0], dir[1]);
 		this.title = title;
 	}
+
+
+	// turning
+
+	static {
+		LEFT.turned = DOWN;
+		DOWN.turned = RIGHT;
+		RIGHT.turned = UP;
+		UP.turned = LEFT;
+	}
+
+	public bool canBeTurned() { return turned != null; }
+	public EdgeType getTurned(int times) throws ChessError {
+		EdgeType type = this;
+		for (int i = 0; i<times; i++) {
+			if (!type.canBeTurned())
+				throw new ChessError("edge can't be turned");
+			type = type.turned;
+		}
+		return type;
+	}
+	public EdgeType getTurned() { return getTurned(1); }
+	public int getTurns(EdgeType toType) {
+		return toType==this? 0: 1 + this.getTurned().getTurns(toType);
+	}
+
+	public static EdgeType[] turnedPath(EdgeType[] path, int turns) {
+		
+	}
+
+
+	// string
 
 	public static EdgeType fromCode(char code) throws ChessError {
 		for (EdgeType t : EdgeType.values())
