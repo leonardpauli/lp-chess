514fcf60add6ebd38568d71bc92bef22c2ed9eb1
Leonard Pauli <leonardpauli@me.com>
2018-09-16 11:48:52 +0200

    Edge: use char as code + getPath

--- src/com/leonardpauli/experiments/boardgame/chess/Edge.java
+++ src/com/leonardpauli/experiments/boardgame/chess/Edge.java
@@ -18,25 +18,32 @@ class Edge {
 }
 
 public enum EdgeType {
-	LEFT ("<", [-1, 0], "left"),
-	RIGHT (">", [1, 0], "right"),
-	UP ("^", [0, 1], "up"),
-	DOWN ("v", [0, -1], "down"),
-	ANY (".", [0, 0], "any");
+	LEFT ('<', {-1, 0}, "left"),
+	RIGHT ('>', {1, 0}, "right"),
+	UP ('^', {0, 1}, "up"),
+	DOWN ('v', {0, -1}, "down"),
+	ANY ('.', {0, 0}, "any");
 
-	public final String code;
+	public final char code;
 	public final Point direction;
-	public final String name;
+	public final String title;
 
-	private EdgeType(String code, int[2] dir, String name) {
+	private EdgeType(char code, int[2] dir, String title) {
 		this.code = code;
 		this.direction = new Point(dir[0], dir[1]);
-		this.name = name;
+		this.title = title;
 	}
 
-	public static EdgeType fromCode(String code) throws ChessError {
+	public static EdgeType fromCode(char code) throws ChessError {
 		for (EdgeType t : EdgeType.values())
-			if (t.code.equals(code)) return t;
+			if (t.code == code) return t;
 		throw ChessError("enum not found");
+	}
+	public static EdgeType[] getPath(String code) throws ChessError {
+		EdgeType[] path = new EdgeType[code.length()];
+		int i = 0;
+		for (char ch : code.toCharArray())
+			path[i++] = fromCode(ch);
+		return path;
 	}
 }
