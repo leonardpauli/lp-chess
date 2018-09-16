1d296d6f9341c7bdf1818e8d362ff724d336cc10
Leonard Pauli <leonardpauli@me.com>
2018-09-16 19:05:09 +0200

    see prev (missed Printer)

--- src/com/leonardpauli/experiments/boardgame/chess/Printer.java
+++ src/com/leonardpauli/experiments/boardgame/chess/Printer.java
@@ -2,7 +2,7 @@ package com.leonardpauli.experiments.boardgame.chess;
 
 public class Printer {
 	public enum Style { PLAIN, PRETTY, PRETTY_WITH_NUMBERS };
-	public static String boardToString(Board bord, Style style) {
+	public static String boardToString(Board board, Style style) {
 		StringBuilder sb = new StringBuilder("");
 		boolean pretty = style==Style.PRETTY || style==Style.PRETTY_WITH_NUMBERS;
 		boolean numbers = style==Style.PRETTY_WITH_NUMBERS;
@@ -10,11 +10,11 @@ public class Printer {
 		if (pretty && numbers) sb.append("  ");
 		if (pretty) sb.append("╭────────────────────────╮\n");
 
-		for (int y = 0; y<size.y; x++) {
+		for (int y = 0; y<board.size.y; y++) {
 			if (pretty && numbers) sb.append(board.tiles[0][y].position.rowString()+" ");
 			if (pretty) sb.append("│");
 
-			for (int x = 0; x<size.x; x++) {
+			for (int x = 0; x<board.size.x; x++) {
 				Tile tile = board.tiles[x][y];
 				if (pretty) sb.append(" ");
 				sb.append(pretty ? tile.toCharPretty(): tile.toChar());
@@ -31,7 +31,7 @@ public class Printer {
 		if (numbers) {
 			sb.append("  ");
 			if (pretty) sb.append(" ");
-			for (int x = 0; x<size.x; x++) {
+			for (int x = 0; x<board.size.x; x++) {
 				Tile tile = board.tiles[x][0];
 				if (pretty) sb.append(" ");
 				sb.append(tile.position.colString());
