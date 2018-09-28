e0d96fa4dde18fb7d2954f030c932e009452474e
Leonard Pauli <leonardpauli@me.com>
2018-09-28 10:45:33 +0200

    notation.test: running

--- src/main/com/leonardpauli/experiments/boardgame/game/notation/Game.java
+++ src/main/com/leonardpauli/experiments/boardgame/game/notation/Game.java
@@ -5,8 +5,8 @@ import com.leonardpauli.experiments.boardgame.game.notation.tokenizer.*;
 import java.util.ArrayList;
 
 public class Game implements Token {
-  public ArrayList<Tag> tags;
-  public ArrayList<Round> rounds;
+  public ArrayList<Tag.Section> tags = new ArrayList<>();
+  public ArrayList<Round> rounds = new ArrayList<>();
 
   public Token[] getInnerTokens() {
     return new Token[] {
@@ -23,9 +23,10 @@ public class Game implements Token {
     AndToken a = (AndToken) at;
     for (Token ats : a.getTokens()) {
       if (ats instanceof OptionalToken) ats = ((OptionalToken) ats).getToken();
+
       RepeatToken rt = (RepeatToken) ats;
       for (Token t : rt.getTokens()) {
-        if (t instanceof Tag) tags.add((Tag) t);
+        if (t instanceof Tag.Section) tags.add((Tag.Section) t);
         else if (t instanceof Round) rounds.add((Round) t);
       }
     }
