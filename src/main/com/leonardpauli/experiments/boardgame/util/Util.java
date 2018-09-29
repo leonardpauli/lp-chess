c5ff079df80bc62af3a8495492f2c1f140b18bf2
Leonard Pauli <leonardpauli@me.com>
2018-09-29 17:07:27 +0200

    utils.stringFromObject: boolean + Optional type

--- src/main/com/leonardpauli/experiments/boardgame/util/Util.java
+++ src/main/com/leonardpauli/experiments/boardgame/util/Util.java
@@ -3,6 +3,7 @@ package com.leonardpauli.experiments.boardgame.util;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
 public class Util {
   public static String objectToString(Object o) {
@@ -16,20 +17,30 @@ public class Util {
     if (fields.length > 0) {
       sb.append("{");
       int i = 0;
+      boolean shouldNotAddComma = true;
       for (Field f : fields) {
-        boolean isFirst = i == 0;
-        if (!isFirst) sb.append(", ");
-        sb.append(f.getName() + ": ");
-
+        if (!shouldNotAddComma) sb.append(", ");
+        shouldNotAddComma = false;
         try {
-          Object val = f.get(o);
-          if (val != null && val.getClass().isArray()) {
-            appendIterable(sb, Arrays.asList((Object[]) val));
-          } else if (val instanceof ArrayList) {
-            appendIterable(sb, (ArrayList) val);
+          if (f.getType().equals(boolean.class)) {
+            if (f.getBoolean(o)) sb.append(f.getName());
+            else shouldNotAddComma = true;
           } else {
-            sb.append(val);
+            sb.append(f.getName() + ": ");
+            Object val = f.get(o);
+
+            if (val != null && val.getClass().isArray()) {
+              appendIterable(sb, Arrays.asList((Object[]) val));
+            } else if (val instanceof ArrayList) {
+              appendIterable(sb, (ArrayList) val);
+            } else if (val instanceof Optional) {
+              if (!((Optional) val).isPresent()) sb.append("Empty");
+              else sb.append(((Optional) val).get());
+            } else {
+              sb.append(val);
+            }
           }
+
         } catch (IllegalAccessException e) {
           throw new RuntimeException(e);
         }
