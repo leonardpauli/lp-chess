e2e145258477396f3dc267d1ba5190b8cdb7e901
Leonard Pauli <leonardpauli@me.com>
2018-09-29 17:08:13 +0200

    tokenizer.utils.AndToken: fix only return matched tokens

--- src/main/com/leonardpauli/experiments/boardgame/game/notation/tokenizer/utils/AndToken.java
+++ src/main/com/leonardpauli/experiments/boardgame/game/notation/tokenizer/utils/AndToken.java
@@ -6,27 +6,33 @@ import com.leonardpauli.experiments.boardgame.game.notation.tokenizer.Tokenizer;
 import com.leonardpauli.experiments.boardgame.game.notation.tokenizer.TokenizerException;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class AndToken implements Token {
 
   private Token[] tokens;
+  private Token[] matchedTokens;
 
   public AndToken(Token[] tokens) {
     this.tokens = tokens;
   }
 
   public Token[] getTokens() {
-    return tokens;
+    return matchedTokens;
   }
 
   public TokenizeResult getMatchResult(Tokenizer tokenizer, int offset, String str)
       throws IOException, TokenizerException {
+    matchedTokens = new Token[tokens.length];
+    int i = 0;
     int consumed = 0;
     for (Token t : tokens) {
       TokenizeResult res = tokenizer.tokenize(t, offset + consumed);
       if (!res.ok && !(t instanceof OptionalToken)) return res;
+      if (res.ok) matchedTokens[i++] = t;
       consumed += res.consumedCount;
     }
+    matchedTokens = Arrays.copyOf(matchedTokens, i);
     return new TokenizeResult(consumed);
   }
 }
