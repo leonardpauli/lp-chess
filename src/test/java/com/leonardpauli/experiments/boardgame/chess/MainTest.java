fe2e51732ae8b8e8c9b0b667037ae0ca56076e44
Leonard Pauli <leonardpauli@me.com>
2018-10-05 22:40:32 +0200

    test: using maven (mvn test)

---
+++ src/test/java/com/leonardpauli/experiments/boardgame/chess/MainTest.java
@@ -0,0 +1,13 @@
+package com.leonardpauli.experiments.boardgame.chess;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+  @Test
+  public void testMain() {
+    assertEquals(2, 1 + 1);
+  }
+}
